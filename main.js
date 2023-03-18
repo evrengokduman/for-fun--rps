bab7ddcd24da38dde8bdc9956e0778ad321d2326
evren <sukruevrengokduman@gmail.com>
2023-03-18 22:59:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    emoji: "✋",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "✌",
+    beats: "paper",
+  },
+];
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener("click", (e) => {
+    const selectionName = selectionButton.dataset.selection;
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === selectionName
+    );
+    makeSelection(selection);
+  });
+});
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection();
+  //if you won
+  const yourWinner = isWinner(selection, computerSelection);
+  //if the computer won
+  const computerWinner = isWinner(computerSelection, selection);
+
+  addSelectionResult(computerSelection, computerWinner);
+  addSelectionResult(selection, yourWinner);
+}
+
+function addSelectionResult(selection, winner) {}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name;
+}
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
+}
