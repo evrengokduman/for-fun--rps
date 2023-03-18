0dbe5b17dc4f9ceccde780d0093492ba8b9508de
evren <sukruevrengokduman@gmail.com>
2023-03-18 23:15:23 +0300

    final changes for ui

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll("[data-selection]");
+const finalColumn = document.querySelector("[data-final-column");
+const computerScoreSpan = document.querySelector("[data-computer-score]");
+const yourScoreSpan = document.querySelector("[data-your-score]");
 const SELECTIONS = [
   {
     name: "rock",
@@ -35,9 +38,22 @@ function makeSelection(selection) {
 
   addSelectionResult(computerSelection, computerWinner);
   addSelectionResult(selection, yourWinner);
+
+  if (yourWinner) incrementScore(yourScoreSpan);
+  if (computerWinner) incrementScore(computerScoreSpan);
 }
 
-function addSelectionResult(selection, winner) {}
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner) {
+  const div = document.createElement("div");
+  div.innerText = selection.emoji;
+  div.classList.add("result-selection");
+  if (winner) div.classList.add("winner");
+  finalColumn.after(div);
+}
 
 function isWinner(selection, opponentSelection) {
   return selection.beats === opponentSelection.name;
